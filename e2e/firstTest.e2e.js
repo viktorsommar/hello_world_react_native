92149418edbd856a751d993751586cec26f0d619
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-19 18:58:31 +0200

    xit test to not run it

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -9,12 +9,12 @@ describe('Example', () => {
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
+  xit('should show hello screen after tap', async () => {
     await element(by.id('hello_button')).tap();
     await expect(element(by.text('Hello!!!'))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
+  xit('should show world screen after tap', async () => {
     await element(by.id('world_button')).tap();
     await expect(element(by.text('World!!!'))).toBeVisible();
   });
